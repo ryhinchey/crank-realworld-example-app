c9acb903807c641d33f0d54f3b10d8bfe533c7eb
Ryan Hinchey <rlhinchey@gmail.com>
2020-05-20 21:34:56 -0400

    adding pagination

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,24 +1,75 @@
-import { createElement } from '@bikeshaving/crank';
+import { createElement, Fragment } from '@bikeshaving/crank';
 import ArticlePreview from './ArticlePreview';
 import api from '../api';
 
-async function ArticleList() {
-  const { articles } = await api.Articles.all();
-  if (articles.length === 0) {
-    return (
-      <div class="article-preview">
-        No articles are here... yet.
-      </div>
-    );
+const ListPagination = ({setPage, articlesCount, currentPage}) => {
+  if (articlesCount <= 10) {
+    return null;
+  }
+
+  let range = [];
+  for (let i = 0; i < Math.ceil(articlesCount / 10); ++i) {
+    range.push(i);
   }
 
   return (
-    <div>
-      {articles.map(article => (
-        <ArticlePreview article={article} key={article.slug} />
-      ))}
-    </div>
-  )
+    <nav>
+      <ul className="pagination">
+        {range.map(v => {
+          const isCurrent = v === currentPage;
+          return (
+            <li class={ isCurrent ? 'page-item active' : 'page-item' }>
+              <button
+                class="page-link"
+                onclick={() => setPage(v)}
+                crank-key={v.toString()}>
+                {v + 1}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+
+function withPagination(Component) {
+  return function *(props) {
+    let page = 0;
+
+    const setPage = p => {
+      page = p;
+      this.refresh();
+    };
+
+    while (true) (
+      yield <Component {...props} page={page} setPage={setPage} />
+    )
+  }
 }
 
-export default ArticleList;
+async function *ArticleList({page, setPage}) {
+  for await ({page, setPage} of this) {
+    const { articles, articlesCount} = await api.Articles.all(page);
+
+    if (articles.length === 0) {
+      yield (
+        <div class="article-preview">
+          No articles are here... yet.
+        </div>
+      );
+    }
+
+    yield (
+      <Fragment>
+        {articles.map(article => (
+          <ArticlePreview article={article} key={article.slug} />
+        ))}
+        <ListPagination setPage={setPage} currentPage={page} articlesCount={articlesCount} />
+      </Fragment>
+    )
+  }
+}
+
+export default withPagination(ArticleList);
